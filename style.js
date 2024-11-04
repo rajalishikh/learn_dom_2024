07bc9b81da268ccb12c4f03addda38e1e34704a6
unknown <nahidujjamanraj@gmail.com>
2024-11-04 12:59:07 +0600

    In this time I konow about documet object model one topic get element by taqname

--- style.js
+++ style.js
@@ -1,2 +1,15 @@
 console.log('hello from js ')
 console.log(document)
+
+let student={
+    name:'raj',
+    roll:22,
+    study:function(study){
+        console.log("study time",study)
+    }
+}
+
+let find_item1=document.getElementsByTagName("li")
+for(let item of find_item1){
+    console.log(item.innerText)
+}
